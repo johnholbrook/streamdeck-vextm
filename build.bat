7c2322ddb7ef035afc596c5564150c90e909b4ab
John Holbrook <3682581+johnholbrook@users.noreply.github.com>
2021-12-29 10:00:14 -0500

    Distribute plugin as .streamDeckPlugin file

---
+++ build.bat
@@ -0,0 +1,11 @@
+@REM build the node script that talks to TM and responds to keypresses
+cd .\us.johnholbrook.vextm.sdPlugin\plugin_node
+call npm install
+call npm run build
+cd ..\..
+
+@REM package the .sdPlugin folder into a .streamDeckPlugin file for distribution
+@REM https://developer.elgato.com/documentation/stream-deck/sdk/exporting-your-plugin/
+mkdir dist
+del .\dist\us.johnholbrook.vextm.streamDeckPlugin
+DistributionTool.exe -b -i us.johnholbrook.vextm.sdPlugin -o .\dist
